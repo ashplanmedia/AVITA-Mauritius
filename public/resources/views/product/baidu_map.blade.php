ce14851174aba54afd4bf0f87bf3d3a193e82a4f
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-05-30 02:17:33 +0530

    Update baidu_map.blade.php

--- public/resources/views/product/baidu_map.blade.php
+++ public/resources/views/product/baidu_map.blade.php
@@ -95,12 +95,13 @@
                         <div id="bmap">
 
                         </div>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29909.961036617693!2d57.66004149052087!3d-20.434566955206332!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x217c8ae8965558c3%3A0x219a58ed6b8a51df!2sPlaine+Magnien%2C+Mauritius!5e0!3m2!1sen!2sin!4v1559162795253!5m2!1sen!2sin" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 
-                        {{--<div class="map-wrap embed-responsive embed-responsive-1by1">--}}
+                      <!------  {{--<div class="map-wrap embed-responsive embed-responsive-1by1">--}}
                             {{--<div id="gmap-embed" class="embed-responsive-item">--}}
 
                             {{--</div>--}}
-                        {{--</div>--}}
+                        {{--</div>--}}------>
                     </div>
 
                 </div>

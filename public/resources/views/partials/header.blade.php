f0df44c5884d0b4c6eb0cf52cdb266788c058d03
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-06-14 20:07:33 +0530

    Update header.blade.php

--- public/resources/views/partials/header.blade.php
+++ public/resources/views/partials/header.blade.php
@@ -58,13 +58,13 @@
                                          @else
 
 
-
+                <!---  
                                             <li class="nav-item">
                                                 <a class="nav-link px-md-4 py-2" href="{{ route('product.overview', ['liber-new-generation']) }}">LIBER New Generation</a>
                                             </li>
 
 
-                                          <!---  <li class="nav-item">
+                                         <li class="nav-item">
                                                 <a class="nav-link px-md-4 py-2" href="{{ route('product.overview', ['liber']) }}">LIBER Series 13.3" & 14"</a>
                                             </li>
                                             ----->

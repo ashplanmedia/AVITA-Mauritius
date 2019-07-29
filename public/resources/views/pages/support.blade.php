b691ea7996f0acfc7ca7c6cbf6e02b79bf965628
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-07-29 13:55:59 +0530

    Update support.blade.php

--- public/resources/views/pages/support.blade.php
+++ public/resources/views/pages/support.blade.php
@@ -104,9 +104,9 @@
                 var location = new google.maps.LatLng( data.lat, data.lng );
 
                 var map = new google.maps.Map(item, {
-                    zoom: 19,
-                    maxZoom: 17,
-                    minZoom: 21,
+                    zoom: 18,
+                    maxZoom: 18,
+                    minZoom: 18,
                     disableDefaultUI: true,
                     center: location,
                 });

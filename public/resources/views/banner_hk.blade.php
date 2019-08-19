ecca871a619f1c9626b5f764abe951560987c898
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-08-19 12:23:03 +0530

    Update banner_hk.blade.php

--- public/resources/views/banner_hk.blade.php
+++ public/resources/views/banner_hk.blade.php
@@ -1,56 +1,58 @@
 <section class="top-nav-padding homepage-banner">
- <div id="home_banner" class="carousel slide" data-ride="carousel" data-interval="0" data-pause="">
+
+    <div id="home_banner" class="carousel slide" data-ride="carousel" data-interval="0" data-pause="">
         <div class="carousel-inner" role="listbox">
 
-            <div class="carousel-item active">
+<div class="carousel-item active">
                 <div class="responsive-block">
                     <div class="banner-block responsive-item">
-<!---<a href="https://www.flipkart.com/search?q=avita+laptop&as=on&as-show=on&otracker=AS_Query_HistoryAutoSuggest_1_1&otracker1=AS_Query_HistoryAutoSuggest_1_1&as-pos=1&as-type=HISTORY&as-searchtext=a">-->
+                        <a href="{{ route('product.overview', 'magus12-2in1-laptop') }}">
                             @if( App::isLocale('en') )
                                 <div class="banner-bg hidden-sm-down"
-                                     style="background-image: url('/images/banner/AVITA-KV_001_green_web_banners_1920x720_v02-en.jpg')"></div>
+                                     style="background-image: url('/images/banner/magus_web.jpg')"></div>
+                            	<div class="banner-bg hidden-md-up"
+                                 style="background-image: url('/images/banner/magus_mob.jpg')"></div>
                             @else
                                 <div class="banner-bg hidden-sm-down"
-                                     style="background-image: url('/images/banner/AVITA-KV_001_green_web_banners_1920x720_v02-tc.jpg')"></div>
+                                     style="background-image: url('/images/banner/magus_web.jpg')"></div>
+                               	<div class="banner-bg hidden-md-up"
+                                 style="background-image: url('/images/banner/magus_mob.jpg')"></div>
+
                             @endif
-                            <div class="banner-bg hidden-md-up"
-                                 style="background-image: url('/images/banner/banner3_mob.jpeg')"></div>
                         </a>
                     </div>
                 </div>
             </div>
-
-            <div class="carousel-item ">
+ 
+            
+         <div class="carousel-item">
                 <div class="responsive-block">
-                    <div class="banner-block responsive-item  d-flex align-items-center">
-                        <video class="leadin-video video-bgv1" muted autoplay playsinline>
-                            <source src="/videos/bgv1.mp4" type="video/mp4">
-                        </video>
-                        <div class="banner-info mb-5 pb-5 w-100 align-self-end">
-                            <div class="btn-group mt-5 pt-5 pt-md-0 ls-0">
-                                <a class="btn btn-more mx-auto mt-5 mt-md-0 font-weight-normal" href="{{ route('product.overview', 'liber') }}">@lang('site.home_learnmore')<i class="fa fa-chevron-right ml-2" aria-hidden="true"></i></a>
-                            </div>
-                        </div>
+                    <div class="banner-block responsive-item">
+                        <a href="{{ route('product.overview', ['liber12']) }}">
+                            @if( App::isLocale('en') )
+                                <div class="banner-bg hidden-sm-down"
+                                     style="background-image: url('/images/banner/Web_Banner.png')"></div>
+                            @else
+                                <div class="banner-bg hidden-sm-down"
+                                     style="background-image: url('/images/banner/Web_Banner.png')"></div>
+                            @endif
+                            <div class="banner-bg hidden-md-up"
+                                 style="background-image: url('/images/banner/Mobile_Banner.png')"></div>
+                        </a>
                     </div>
                 </div>
             </div>
-
-            <div class="carousel-item">
+            
+              <div class="carousel-item">
                 <div class="responsive-block">
                     <div class="banner-block responsive-item d-flex align-items-center">
                         <video class="leadin-video video-bgv2" muted playsinline>
-                            <source src="/videos/bgv2.mp4" type="video/mp4">
+                            <source src="/videos/magus.mp4" type="video/mp4">
                         </video>
-                        <div class="banner-info mb-5 pb-5 w-100 align-self-end">
-                            <div class="btn-group mt-5 pt-5 pt-md-0 ls-0">
-                                <a class="btn btn-more mx-auto mt-5 mt-md-0 font-weight-normal" href="{{ route('product.overview', 'liber') }}">@lang('site.home_learnmore')<i class="fa fa-chevron-right ml-2" aria-hidden="true"></i></a>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
-
-        </div>
+  
         <ol class="carousel-indicators">
             <li data-target="#home_banner" data-slide-to="0" class="active"></li>
             <li data-target="#home_banner" data-slide-to="1"></li>
@@ -58,6 +60,7 @@
         </ol>
     </div>
 </section>
+
 
 @section('css')
     <style type="text/css">

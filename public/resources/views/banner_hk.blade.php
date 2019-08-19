eaa4bec8ab66d5a479eccab128a28fc069341fd4
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-08-19 12:05:04 +0530

    Update banner_hk.blade.php

--- public/resources/views/banner_hk.blade.php
+++ public/resources/views/banner_hk.blade.php
@@ -47,7 +47,7 @@
                 <div class="responsive-block">
                     <div class="banner-block responsive-item d-flex align-items-center">
                         <video class="leadin-video video-bgv2" muted playsinline>
-                            <source src="/videos/magus.mp4" type="video/mp4">
+                            <source src="/videos/magus.mp4" type="video/mp4" controls autoplay>
                         </video>
                     </div>
                 </div>

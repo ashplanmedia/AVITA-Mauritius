d2b74cbb638c05ab3324240865c11786d0630f2d
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-10-09 16:16:42 +0530

    Update liber_feature.blade.php

--- public/resources/views/product/liber_feature.blade.php
+++ public/resources/views/product/liber_feature.blade.php
@@ -13,7 +13,7 @@
             <div class="responsive-block">
                 <div class="banner-block responsive-item">
                     <div class="banner-bg hidden-sm-down" style="background-image: url('/images/banner/banner.jpg')"></div>
-                    <div class="banner-bg hidden-md-up" style="background-image: url('/images/banner/banner_mob.jpg')"></div>
+                    <div class="banner-bg hidden-md-up" style="background-image: url('/images/banner/banner_mobi.jpg')"></div>
                     <div class="banner-info">
                         <div class="btn-group mt-3">
                             {{--<img class="mx-auto" src="/images/liber/brand.png">--}}
